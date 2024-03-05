8e43cbe2dbdd961547aa2b293ff602d12157da05
sonkey <annagu.kennedy@gmail.com>
2024-03-05 01:05:03 -0700

    Minor bug

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://apartment-beta.vercel.app/",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type"],
   })
